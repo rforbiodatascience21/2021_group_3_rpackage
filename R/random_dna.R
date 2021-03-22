eb8b2dd93eed28a620b99429636b94e0a5d259a8
kiaraszakter <szakterkiara@gmail.com>
2021-03-22 09:35:40 +0000

    documentation

--- R/random_dna.R
+++ R/random_dna.R
@@ -1,4 +1,6 @@
-# R/random_dna.R
+#' generated random DNA of desired size
+#' @export
+
 random_dna <- function(l){
   nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
   dna = paste0(nucleotides, collapse = "")
