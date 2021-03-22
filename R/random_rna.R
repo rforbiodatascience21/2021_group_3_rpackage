1a5eefe99794ce30dfb8083a9a742cea10e702fd
AugustEhl <august.ehlers@gmail.com>
2021-03-22 10:56:19 +0000

    random_rna documented

--- R/random_rna.R
+++ R/random_rna.R
@@ -1,4 +1,9 @@
-# R/random_rna.R
+#' Random RNA Generator
+#'
+#' Given a length "l", create a random sequence of length "l"
+#'
+#' @return None
+#' @export
 random_rna <- function(l){
   nucleotides <- sample(c("A", "U", "G", "C"), size = l, replace = TRUE)
   dna = paste0(nucleotides, collapse = "")
