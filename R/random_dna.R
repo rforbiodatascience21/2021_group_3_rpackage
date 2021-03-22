d9462c873eede73152f1ffe5b577e11a09c184b9
kiaraszakter <szakterkiara@gmail.com>
2021-03-22 10:00:16 +0000

    delete

--- R/random_dna.R
+++ R/random_dna.R
@@ -5,13 +5,5 @@ random_dna <- function(l){
   nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
   dna = paste0(nucleotides, collapse = "")
   return(dna)
-<<<<<<< HEAD
-}
-=======
-<<<<<<< HEAD
 }
 
-=======
-}
->>>>>>> e9fa2ee279266713f8419f8d1bed1b3601c28d4f
->>>>>>> 99b6e207df3d215485f65acc755de7d4fd7109a3
