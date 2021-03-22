1a293fcc29d484a73a36cc5822a673a8a4d9bcb7
HCEhlers <holgerehlers@hotmail.com>
2021-03-22 09:47:22 +0000

    committing before push

--- R/random_dna.R
+++ R/random_dna.R
@@ -3,5 +3,9 @@ random_dna <- function(l){
   nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
   dna = paste0(nucleotides, collapse = "")
   return(dna)
+<<<<<<< HEAD
 }
 
+=======
+}
+>>>>>>> e9fa2ee279266713f8419f8d1bed1b3601c28d4f
